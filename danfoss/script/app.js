9424d9c55f0d963a3a08f14f7bde54c2fa913837
BUDAPEST\Zsolt_Kakuk <zsolt.kakuk@catenamedia.com>
2018-04-21 13:29:34 +0200

    Adjusted more on homepage (parallax Careers bg,...)

--- danfoss/script/app.js
+++ danfoss/script/app.js
@@ -1,20 +1,14 @@
 $(function() {
 	var parallax;
 	$(document).on('scroll', function() {
-		parallax = parallax || $('.bg-building');
+		parallax = parallax || $('.bg-building,.bg-careers');
 		parallax.each(function(idx, el) {
 			var $el = $(el);
-			var data = $el.data('bgimg');
-			if (!data) {
-				var imageSrc = $el.css('background-image').replace(/url\((['"])?(.*?)\1\)/gi, '$2').split(',')[0];
-				var image = new Image();
-				image.src = imageSrc;
-				data = {
-					width: image.width
-				};
-				$el.data('bgimg', data);
-			}
-			$(el).css('background-position-x', Math.min($el.offset().top / 8 - $(window).scrollTop() / 2, 0));
+			$el.css('background-position-x',
+				$el.hasClass('bg-building')
+				? ~~Math.min($el.offset().top / 8 - $(window).scrollTop() / 2, 0)
+				: ~~Math.min($el.offset().top / 8 - $(window).scrollTop() / 4.5, 0)
+			);
 		});
 	}).trigger('scroll');
 });
